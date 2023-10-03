b43e8987a8fc5cae5ba01aac1ade6d11b5f22a06
seungchae <id6639@naver.com>
2023-10-04 03:36:40 +0900

    Protecting routes, Profiles screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
+import React from "react";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
+
   return (
     <>
       <h1 className="text-2xl text-red-500">Netflix-Clone</h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>LogOut</button>
     </>
   );
 }
