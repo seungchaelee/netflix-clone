ca973227fe1c93ecaba16368cdc132d7c9709366
seungchae <id6639@naver.com>
2023-10-30 18:59:45 +0900

    favorites / My List

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -1,77 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { without } from "lodash";
-
-// import prismadb from "@/lib/prismadb";
-// import serverAuth from "@/lib/serverAuth";
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     if (req.method === "POST") {
-//       const { currentUser } = await serverAuth(req);
-
-//       const { movieId } = req.body;
-
-//       const existingMovie = await prismadb.movie.findUnique({
-//         where: {
-//           id: movieId,
-//         }
-//       });
-
-//       if (!existingMovie) {
-//         throw new Error("Invalid ID");
-//       }
-
-//       const user = await prismadb.user.update({
-//         where: {
-//           email: currentUser.email || "",
-//         },
-//         data: {
-//           favoriteIds: {
-//             push: movieId
-//           }
-//         }
-//       });
-
-//       return res.status(200).json(user);
-//     }
-
-//     if (req.method === "DELETE") {
-//       const { currentUser } = await serverAuth(req);
-
-//       const { movieId } = req.body;
-
-//       const existingMovie = await prismadb.movie.findUnique({
-//         where: {
-//           id: movieId,
-//         }
-//       });
-
-//       if (!existingMovie) {
-//         throw new Error("Invalid ID");
-//       }
-
-//       const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
-
-//       const updatedUser = await prismadb.user.update({
-//         where: {
-//           email: currentUser.email || "",
-//         },
-//         data: {
-//           favoriteIds: updatedFavoriteIds,
-//         }
-
-//       });
-
-//       return res.status(200).json(updatedUser);
-//     }
-
-//     return res.status(405).end();
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(400).end();
-//   }
-// }
-
 import { NextApiRequest, NextApiResponse } from "next";
 import { without } from "lodash";
 
@@ -109,34 +35,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(200).json(user);
     }
 
-    // if (req.method === "DELETE") {
-    //   const { currentUser } = await serverAuth(req);
+    if (req.method === "DELETE") {
+      const { currentUser } = await serverAuth(req);
 
-    //   const { movieId } = req.body;
+      const { movieId } = req.body;
 
-    //   const existingMovie = await prismadb.movie.findUnique({
-    //     where: {
-    //       id: movieId,
-    //     }
-    //   });
+      const existingMovie = await prismadb.movie.findUnique({
+        where: {
+          id: movieId,
+        }
+      });
 
-    //   if (!existingMovie) {
-    //     throw new Error("Invalid ID");
-    //   }
+      if (!existingMovie) {
+        throw new Error("Invalid ID");
+      }
 
-    //   const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+      const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
 
-    //   const updatedUser = await prismadb.user.update({
-    //     where: {
-    //       email: currentUser.email || "",
-    //     },
-    //     data: {
-    //       favoriteIds: updatedFavoriteIds,
-    //     }
-    //   });
+      const updatedUser = await prismadb.user.update({
+        where: {
+          email: currentUser.email || "",
+        },
+        data: {
+          favoriteIds: updatedFavoriteIds,
+        }
+      });
 
-    // return res.status(200).json(updatedUser);
-    // }
+    return res.status(200).json(updatedUser);
+    }
 
     return res.status(405).end();
   } catch (error) {
